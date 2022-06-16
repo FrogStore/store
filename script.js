dcbce11cb33da9d55ef0c438db618eec7f734d6f
Frog Store <107469363+FrogStore@users.noreply.github.com>
2022-06-16 21:03:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+var p12 = "15440c270454"
+var mobileprovision = "5810b5df32f2"
+var password = "123k"
+
+
+function starfiles() {
+window.open("https://www.starfiles.co/","_self")
+}
+
+
+function unc0ver() {
+window.open("https://sign.starfiles.co/?ipa=1bca09a52a24&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function oddesy() {
+window.open("https://sign.starfiles.co/?ipa=91ee2aeface2&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function taurine() {
+window.open("https://sign.starfiles.co/?ipa=7b47d621be73&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function animator() {
+window.open("https://sign.starfiles.co/?ipa=6a70f2177ffb&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function delta() {
+window.open("https://sign.starfiles.co/?ipa=5c8205d85428&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function inds() {
+window.open("https://sign.starfiles.co/?ipa=497d4820373b&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+function flappybird() {
+window.open("https://sign.starfiles.co/?ipa=5c713ce0a908&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function itorrent() {
+window.open("https://sign.starfiles.co/?ipa=8c3c5ec067f4&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function provence() {
+window.open("https://sign.starfiles.co/?ipa=1e9e033a479c&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function utm() {
+window.open("https://sign.starfiles.co/?ipa=4015857d85b2&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function spotify() {
+window.open("https://sign.starfiles.co/?ipa=ac57bcb2a36d&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function ipogo() {
+window.open("https://sign.starfiles.co/?ipa=23ec9eb6d9a8&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function filza() {
+window.open("https://sign.starfiles.co/?ipa=9b2fc1e786d6&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function nulls() {
+window.open("https://sign.starfiles.co/?ipa=9585f096878a&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function subwaysurfer() {
+window.open("https://sign.starfiles.co/?ipa=d71786268a47&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function ppsspp() {
+window.open("https://sign.starfiles.co/?ipa=7b1dd434a5a0&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+
+function sign() {
+var x = document.getElementById("ipa").value;
+window.open("https://sign.starfiles.co/?ipa=" + ipa + "&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
+console.log("good")
